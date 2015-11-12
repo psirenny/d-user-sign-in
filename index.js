476108413d7d43cc83c4157a136911499a20956c
psirenny <djtorres0@gmail.com>
2015-11-12 14:25:36 -0500

    clear form data on reset

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ Component.prototype.focus = function () {
 
 Component.prototype.reset = function () {
   if (this.form) this.form.reset();
+  this.model.del('data');
   this.model.del('error');
   this.model.del('submitting');
 };
